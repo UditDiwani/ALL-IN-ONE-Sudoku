95fa0edb93ff464633a619d378254e429c2ff023
UditDiwani <uditdiwani@gmail.com>
2026-02-10 09:34:08 +0530

    updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',   // live server
   'http://localhost:5500',   // some setups use localhost
   "null",                      // allow file:// (no origin)
-  "https://uditdiwani.github.io/",
+  "https://uditdiwani.github.io",
 ];
 
 app.use(cors({
